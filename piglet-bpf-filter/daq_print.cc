c0094f5ff0a9950ac44dd1968893efff6c40e6b9
Victor Roemer <victor@badsec.org>
2024-08-08 19:17:12 -0500

    piglet wrapper

---
+++ piglet-bpf-filter/daq_print.cc
@@ -0,0 +1,228 @@
+// print_packet, coppied from many of my tools using libpacket
+// Victor Roemer, wtfbbqhax <viroemer@badsec.org>
+
+#include <cstdio>
+#include <cstdint>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <daq.h>
+#include <packet/packet.h>
+#include <packet/stats.h>
+#include <packet/dns.h>
+
+#include "daq_print.h"
+
+#define IS_SET(flags, bit) ((flags & bit) == bit)
+
+// From https://github.com/the-tcpdump-group/tcpdump/blob/master/nameser.h#L312
+
+/*
+ * Macros for subfields of flag fields.
+ */
+#define DNS_QR(flags)		((flags) & 0x8000)	/* response flag */
+#define DNS_OPCODE(flags)	(((flags) >> 11) & 0xF)	/* purpose of message */
+#define DNS_AA(flags)		(flags & 0x0400)	/* authoritative answer */
+#define DNS_TC(flags)		(flags & 0x0200)	/* truncated message */
+#define DNS_RD(flags)		(flags & 0x0100)	/* recursion desired */
+#define DNS_RA(flags)		(flags & 0x0080)	/* recursion available */
+#define DNS_AD(flags)		(flags & 0x0020)	/* authentic data from named */
+#define DNS_CD(flags)		(flags & 0x0010)	/* checking disabled by resolver */
+#define DNS_RCODE(flags)	(flags & 0x000F)	/* response code */
+
+// Function to decode the DNS protocol
+int print_dns(dns const& dns)
+{
+    bool is_response = DNS_QR(dns.h.flags);
+
+    if (is_response)
+    {
+            printf("[dns response] [rcode:%d, id:%d, qdcount: %d, ancount: %d, nscount: %d, arcount:%d]\n",
+            DNS_RCODE(dns.h.flags),
+            dns.h.id,
+            dns.h.qdcount,
+            dns.h.ancount,
+            dns.h.nscount,
+            dns.h.arcount);
+    }
+    else
+    {
+            printf("[dns query] [id:%d, qdcount: %d]\n",
+            dns.h.id,
+            dns.h.qdcount);
+    }
+
+    // Parsing Question Section
+    for (int i = 0; i < dns.h.qdcount; i++)
+    {
+        struct dns_query const& q = dns.questions[i];
+        // FIXME: Formatting/printing the QNAME
+        //struct dns_query *q = &dns.questions[i];
+        // Display QNAME
+        // Display QTYPE and QCLASS (assuming a structure in Packet to store this information)
+        printf("[query] [label: %s, type: %d, class: %d]\n",
+                q.label.c_str(),
+                q.dns_qtype,
+                q.dns_qclass);
+    }
+
+    // Parsing Answers Section
+    for (int i = 0; i < dns.h.ancount; i++)
+    {
+        char addr[INET6_ADDRSTRLEN];
+        std::string human;
+ 
+        struct dns_answer const &a = dns.answers[i];
+        if (a.dns_atype == 1)
+        {
+            inet_ntop(AF_INET, a.data.data(), addr, sizeof(addr));
+            human.append(addr, strnlen(addr, INET6_ADDRSTRLEN));
+
+        }
+        else
+        {
+            human = a.data;
+        }
+        printf("[answer] [data: %s, type: %d, class %d, ttl: %d]\n",
+                human.c_str(),
+                a.dns_atype,
+                a.dns_aclass,
+                a.dns_ttl);
+    //    // Display QNAME
+    //    // Display QTYPE and QCLASS (assuming a structure in Packet to store this information)
+    //    (void)dns[0].questions[i].dns_qtype;
+    //    (void)dns[0].questions[i].dns_qclass;
+    }
+ 
+    return 0;
+}
+
+// Taken from Pcapstats BSD License
+//
+void print_data(uint8_t const * data, int64_t length)
+{
+    int i, x, j, c;
+    int w = 0;
+
+    for( i=0; length>0; length -= 16 )
+    {
+        c = length >= 16 ? 16 : length;
+        printf("%06X  ", w);
+        w+=16;
+
+        for( j=0; j<c; j++ )
+            printf("%2.02X ", data[i+j]);
+
+        for( x = length; x<16; x++ )
+            printf("   ");
+
+        for( j=0; j<c; j++ )
+            printf("%c", (isprint(data[i+j]) ? data[i+j] : '.'));
+
+        printf("\n");
+        i+=c;
+    }
+}
+
+
+void
+print_packet(int const instance_id, DAQ_PktHdr_t const* hdr, uint8_t const * data, size_t const len)
+{
+    Packet packet;
+
+    packet_clear(&packet);
+
+    int error = packet_decode(&packet, data, len);
+    (void)error;
+
+    struct ipaddr src = packet_srcaddr(&packet);
+    struct ipaddr dst = packet_dstaddr(&packet);
+    struct ipaddr *a = &src;
+    struct ipaddr *b = &dst;
+    uint32_t sport = packet_srcport(&packet);
+    uint32_t dport = packet_dstport(&packet);
+    uint32_t proto = packet_protocol(&packet);
+
+#ifdef PRINT_PACKET_LAYERS
+    // Extra debug logging
+    unsigned it;
+    for (Protocol *_p = packet_proto_first(&packet, &it);
+            _p; _p = packet_proto_next(&packet, &it))
+    {
+        printf("%s:", packet_proto_name(_p));
+    }
+    printf("\n");
+#endif
+
+    int version = packet_version(&packet);
+    int af = AF_INET;
+    if (version == 6) {
+        af = AF_INET6;
+    }
+
+    char addr[INET6_ADDRSTRLEN];
+    printf("[src: %s, ", inet_ntop(af, a, addr, sizeof(addr)));
+    printf("dst: %s, ", inet_ntop(af, b, addr, sizeof(addr)));
+    printf("proto: %d, id: %d, sp: %d, dp: %d, dlen: %u] [instance: %d]\n",
+            proto,
+            packet_id(&packet),
+            sport,
+            dport,
+            packet_paysize(&packet),
+            instance_id);
+
+    if (packet_is_fragment(&packet))
+    {
+        printf(" |ip off: %u %s\n",
+                packet_frag_offset(&packet),
+                packet_frag_mf(&packet) ? "mf" : "");
+    }
+
+    if (sport == 53 || dport == 53)
+    {
+        dns _dns;
+        decode_dns(packet_payload(&packet),
+                packet_paysize(&packet),
+                &_dns);
+        print_dns(_dns);
+    }
+
+    uint32_t max = packet_paysize(&packet);
+    const uint8_t *payload = packet_payload(&packet);
+    max = max > 128 ? 128 : max;
+    print_data(payload, max);
+
+    //print_data(data, len);
+
+#ifdef PRINT_PACKET_STATS
+    // Packet stats are useful for determining decoding errors
+    struct packet_stats const * stats;
+    packet_stats(&stats);
+
+    printf("ip4 headers: %u\n"
+           "ip4 badsum: %u\n"
+           "ip4 tooshort: %u\n"
+           "ip4 toosmall: %u\n"
+           "ip4 badhlen: %u\n"
+           "ip4 badlen: %u\n",
+            stats->ips_packets,
+            stats->ips_badsum,
+            stats->ips_tooshort,
+            stats->ips_toosmall,
+            stats->ips_badhlen,
+            stats->ips_badlen);
+    printf("tcp headers: %u\n"
+           "tcp badsum: %u\n"
+           "tcp badoff: %u\n"
+           "tcp tooshort %u\n",
+            stats->tcps_packets,
+            stats->tcps_badsum,
+            stats->tcps_badoff,
+            stats->tcps_tooshort);
+#endif
+}
+
+
+
+
---
+++ piglet-bpf-filter/piglet-bpf-filter.cc
@@ -0,0 +1,532 @@
+// DAQ Global - vjr
+#include <sys/un.h>
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+#include <pthread.h>
+
+#include <array>
+#include <atomic>
+#include <cstdio>
+#include <functional>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#include <daq.h>
+#include <daq_module_api.h>
+#include <packet.h>
+#include <packet/dns.h>
+#include <pcap.h>
+
+//#include <sfbpf.h>
+//#include <sfbpf_dlt.h>
+
+
+#include "daq_print.h"
+
+/* DLT_RAW
+ *
+ * Mandatory, as the VPP daq only supports L3 delivery. 
+ *
+ * This is used to set the "base protocol" used in libpcap and libpacket
+ * features. 
+ */
+//#define DLT_RAW 12
+
+/* DAQ_BATCH_SIZE 
+ *
+ * The maximum number of packets (DAQ_Msg_h) that we will batch read/process at
+ * once.
+ */
+#define DAQ_BATCH_SIZE 16
+
+/* SNAPLEN
+ *
+ *  The SNAPLEN is the absolulte maximum size packet we support processing. 
+ *  NOTICE: This value should be defined by VPP as the vlib buffer size.
+ */
+#define SNAPLEN 2048
+
+/* TIMEOUT
+ *
+ * The TIMEOUT amount of usec waiting for daq_receive to return. 
+ * NOTICE: TIMEOUT support in vpp DAQ only works in interrupt mode.
+ */
+#define TIMEOUT 100
+
+/* STATIC_MODULES
+ *
+ *  Enables the support of builtin libdaq_static_vpp.la and libdaq_static_geneve.la
+ *
+ * If you're in a pinch and need to build an all-in-one static binary, you can
+ * do it, but not using the Makefile. */
+#undef STATIC_MODULES
+
+#define UNUSED(name) name ## _unused __attribute__ ((unused))
+#define IS_SET(test, bits) (((test) & (bits)) == (bits))
+
+#ifndef UNIX_PATH_MAX
+#define UNIX_PATH_MAX (sizeof(((struct sockaddr_un*)NULL)->sun_path))
+#endif
+
+using socketpath_t = char[UNIX_PATH_MAX];
+
+#define ERRBUF_SIZE 256
+using Errbuf = std::array<char, ERRBUF_SIZE>;
+
+// DaqVariable
+using DaqVariable = std::pair<std::string, std::string>;
+using DaqVars = std::vector<DaqVariable>;
+
+char const* str_from_verdict(DAQ_Verdict const& verdict);
+
+namespace DAQ
+{
+#ifdef STATIC_MODULES
+    // from libdaq_static_vpp.a
+    extern "C" const DAQ_ModuleAPI_t vpp_daq_module_data;
+    extern "C" const DAQ_ModuleAPI_t geneve_daq_module_data;
+
+    static DAQ_Module_h static_modules[] =
+    {
+        &vpp_daq_module_data,
+        &geneve_daq_module_data,
+        nullptr
+    };
+#endif
+
+    static char const *module_paths[] =
+    {
+        // Location of all the open source Snort daqs (bpf)
+        "/usr/local/lib/daq",
+        nullptr
+    };
+
+    static bool s_modules_loaded = false;
+    void load_modules()
+    {
+        if (s_modules_loaded == false)
+        {
+            s_modules_loaded = true;
+#ifdef STATIC_MODULES
+            daq_load_static_modules(static_modules);
+#endif
+            daq_load_dynamic_modules(module_paths);
+        }
+    }
+
+    void unload_modules()
+    {
+        if (s_modules_loaded == true)
+        {
+            daq_unload_modules();
+            s_modules_loaded = false;
+        }
+    }
+}
+
+// DaqConfig
+//
+// DaqConfig is a "copy safe" representation of a DAQ_Config_h opaque pointer.
+// This would be trivial to implement if the DAQ_Config_t type was avaiable
+// from the DAQ API.
+//
+// The DaqConfig attempts to make a mutable representation of a DAQ_Config_h.
+// To accomplish this task, it implements a factory pattern, constructring C++
+// type DAQ_Config_p that provides a constructor/desctructor for DAQ_Config_h.
+//
+// NOTICE
+//
+//  * Implementing this would be easier if the DAQ_Config_t was not hidden from
+//    the DAQ API.
+//
+class DaqConfig
+{
+public:
+    DaqConfig(std::string module, std::string input, DAQ_Mode mode, DaqVars const & vars)
+        : module(module),
+          input(input),
+          mode(mode),
+          vars(vars)
+    { }
+
+    struct DAQ_Config_p
+    {
+        DAQ_Config_p(std::string module, std::string input, DAQ_Mode mode, DaqVars const & vars)
+            : config(nullptr)
+        {
+            daq_config_new(&config);
+            daq_config_set_input(config, input.c_str());
+            daq_config_set_snaplen(config, SNAPLEN);
+            daq_config_set_timeout(config, TIMEOUT);
+
+            DAQ_Module_h mod = daq_find_module(module.c_str());
+            if (mod == nullptr) {
+                daq_config_destroy(config);
+                config = nullptr;
+            }
+
+            DAQ_ModuleConfig_h modconf = nullptr;
+            int result = daq_module_config_new(&modconf, mod);
+            if (result != DAQ_SUCCESS) {
+                daq_config_destroy(config);
+                config = nullptr;
+            }
+
+            daq_module_config_set_mode(modconf, mode);
+            for (auto const & var : vars) {
+                daq_module_config_set_variable(modconf, var.first.c_str(), var.second.c_str());
+            }
+
+            result = daq_config_push_module_config(config, modconf);
+            if (result != DAQ_SUCCESS) {
+                // NOTICE This is the only time we are allowed to call this ourselves.
+                daq_module_config_destroy(modconf);
+                modconf = nullptr;
+
+                daq_config_destroy(config);
+                config = nullptr;
+            }
+        }
+
+        ~DAQ_Config_p()
+        {
+            // Calling daq_config_destroy will call
+            // `daq_module_config_destroy(modcfg)` on each module.
+            daq_config_destroy(config);
+            config = nullptr;
+        }
+
+        DAQ_Config_h config;
+    };
+
+    DAQ_Config_p get_config() const
+    {
+        return DAQ_Config_p(module, input, mode, vars);
+    }
+
+private:
+    std::string module;
+    std::string input;
+    DAQ_Mode mode;
+    DaqVars vars;
+};
+
+#define FRAME_SIZE 256
+
+struct DaqMsgFrame
+{
+    std::array<DAQ_Msg_h, FRAME_SIZE> msgs;
+    size_t recv_count;
+};
+
+struct DaqVerdictFrame
+{
+    std::array<DAQ_Verdict, FRAME_SIZE> verdicts;
+};
+
+struct RecvResult
+{
+    DAQ_RecvStatus status;
+    DaqMsgFrame const& frame;
+};
+
+class DaqInstance
+{
+public:
+    DaqInstance(DaqConfig const & config)
+        : instance(NULL), config(config)
+    {}
+
+    ~DaqInstance()
+    {
+        if (instance) {
+            daq_instance_destroy(instance);
+            instance = nullptr;
+        }
+
+        msgs.recv_count = 0;
+    }
+
+    DaqInstance(DaqInstance const & other)
+        : config(other.config)
+    { }
+
+    int instantiate()
+    {
+        return daq_instance_instantiate(
+                config.get_config().config,
+                &instance,
+                errbuf.data(),
+                errbuf.size());
+    }
+
+    int start()
+    {
+        return daq_instance_start(instance);
+    }
+
+    int stop()
+    {
+        return daq_instance_stop(instance);
+    }
+
+    RecvResult receive_msgs()
+    {
+        DAQ_RecvStatus rstat;
+        msgs.recv_count = daq_instance_msg_receive(
+                instance,
+                msgs.msgs.max_size(),
+                msgs.msgs.data(),
+                &rstat);
+
+        return { rstat, msgs };
+    }
+
+    //void finalize_msgs(DAQ_Verdict const & verdict)
+    void finalize_msgs(DaqVerdictFrame const & verdicts)
+    {
+        for (unsigned i = 0; i < msgs.recv_count; i++)
+        {
+            DAQ_Msg_h msg = msgs.msgs[i];
+            DAQ_Verdict verdict = verdicts.verdicts[i];
+            daq_instance_msg_finalize(instance, msg, verdict);
+        }
+
+        msgs.recv_count = 0;
+    }
+
+    DAQ_Stats_t get_stats() const
+    {
+        DAQ_Stats_t stats;
+        daq_instance_get_stats(instance, &stats);
+        return stats;
+    }
+
+    void reset_stats() const
+    {
+        daq_instance_reset_stats(instance);
+    }
+
+private:
+    DAQ_Instance_h instance;
+    DaqConfig config;
+    DaqMsgFrame msgs;
+    Errbuf errbuf;
+};
+
+class DataPlaneWorker
+{
+    using threadname_t = char[256];
+
+public:
+    DataPlaneWorker(DaqConfig config, unsigned id, std::string filter, DAQ_Verdict verdict, DAQ_Verdict default_verdict)
+        : config(config),
+          id(id),
+          match_verdict(verdict),
+          default_verdict(default_verdict)
+    {
+        if (pcap_compile_nopcap(SNAPLEN, DLT_EN10MB, &fcode, filter.c_str(), 0, PCAP_NETMASK_UNKNOWN) == -1)
+        {
+            fprintf(stderr, "%s: BPF state machine compilation failed!", __func__);
+            abort();
+        }
+
+        int result = bpf_validate(fcode.bf_insns, fcode.bf_len);
+        if (result != 1)
+        {
+            fprintf(stderr, "%s: BPF is not valid!", __func__);
+            abort();
+        }
+
+        thread = std::thread(&DataPlaneWorker::eval, this);
+        pthread_t native = thread.native_handle();
+
+        snprintf(name, sizeof(name), "pkt_wk_%u", id);
+        pthread_setname_np(native, name);
+
+        // Float these workers on cpus 3 & 4
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(3, &cpuset);
+        CPU_SET(4, &cpuset);
+        pthread_setaffinity_np(native, sizeof(cpuset), &cpuset);
+    }
+
+    ~DataPlaneWorker()
+    {
+        pcap_freecode(&fcode);
+    }
+
+    void join()
+    {
+        thread.join();
+    }
+
+    void eval()
+    {
+        DaqInstance in(config);
+
+        in.instantiate();
+        in.start();
+
+        state = START;
+
+        do {
+            auto recv = in.receive_msgs();
+
+            if (recv.frame.recv_count > 0) {
+                print_packets(recv.frame);
+            }
+
+            if (recv.status == DAQ_RSTAT_ERROR ||
+                recv.status == DAQ_RSTAT_INVALID) {
+                state = STOP;
+            }
+
+            in.finalize_msgs(verdicts);
+
+        } while(state != STOP);
+
+        in.stop();
+    }
+
+    void stop()
+    {
+        state = STOP;
+    }
+
+private:
+    bool filter_packet(UNUSED(DAQ_PktHdr_t const* hdr),
+            uint8_t const* data,
+            uint32_t const size,
+            bpf_program const& fcode)
+    {
+        return bpf_filter(fcode.bf_insns, data, size, size) == SNAPLEN;
+    }
+
+    char const* str_from_verdict(DAQ_Verdict const& verdict)
+    {
+        if (verdict == DAQ_VERDICT_PASS)
+            return "\e[34m" "pass" "\e[0m";
+        if (verdict == DAQ_VERDICT_BLOCK)
+            return "\e[31m" "block" "\e[0m";
+        if (verdict == DAQ_VERDICT_WHITELIST)
+            return "\e[32m" "allowlist" "\e[0m";
+        if (verdict == DAQ_VERDICT_BLACKLIST)
+            return "\e[35m" "blocklist" "\e[0m";
+        if (verdict == DAQ_VERDICT_IGNORE)
+            return "ignore";
+        return "";
+    }
+
+    void print_packets(DaqMsgFrame const& frame)
+    {
+        for (unsigned i = 0; i < frame.recv_count; i++)
+        {
+            auto const & msg = frame.msgs[i];
+            if (msg->type == DAQ_MSG_TYPE_PACKET) {
+                DAQ_PktHdr_t const * hdr = daq_msg_get_pkthdr(msg);
+                uint8_t const * data = daq_msg_get_data(msg);
+                uint32_t const size = daq_msg_get_data_len(msg);
+
+                DAQ_Verdict verdict = default_verdict;
+                bool matched = false;
+                if (filter_packet(hdr, data, size, fcode)) {
+                    verdict = match_verdict;
+                    matched = true;
+                }
+
+                verdicts.verdicts[i] = verdict;
+                printf(matched ? "[match|%s] " : "[%s]", str_from_verdict(verdict));
+                print_packet(id, hdr, data, hdr->pktlen);
+            }
+        }
+    }
+
+    DaqConfig config;
+    unsigned id;
+
+    enum { INVAL, STOP, START } state;
+    std::thread thread;
+    threadname_t name;
+    bpf_program fcode;
+    DaqVerdictFrame verdicts;
+
+    DAQ_Verdict match_verdict;
+    DAQ_Verdict default_verdict = DAQ_VERDICT_PASS;
+};
+
+// this is similar to how tcpdump 
+static inline std::string
+concat_args(int argc, char const* argv[])
+{
+    std::string result;
+    for (int i = 0; i < argc; ++i)
+    {
+        if (i > 0)
+        {
+            result += " ";
+        }
+        result += argv[i];
+    }
+    return result;
+}
+
+static inline DAQ_Verdict
+verdict_from_str(std::string const& arg)
+{
+    if (arg == "pass")
+        return DAQ_VERDICT_PASS;
+
+    if (arg == "block")
+        return DAQ_VERDICT_BLOCK;
+
+    if (arg == "allowlist" || arg == "whitelist")
+        return DAQ_VERDICT_WHITELIST;
+
+    if (arg == "blocklist" || arg == "blacklist")
+        return DAQ_VERDICT_BLACKLIST;
+
+    abort();
+    return DAQ_VERDICT_IGNORE;
+}
+
+int main(int argc, char const* argv[])
+{
+    DAQ::load_modules();
+    packet_set_datalink(DLT_EN10MB);
+
+    //socketpath_t socket_path {};
+    //snprintf(socket_path, sizeof(socket_path), "/tmp/snort.sock");
+
+    DaqVars vars {
+        //{ "socket_path", socket_path },
+        //{ "debug", "true" },
+    };
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: piglet-bpf-filter [pass|block|allowlist|blocklist] <BPF expression>\n");
+        exit(1);
+    }
+
+    DAQ_Verdict default_verdict = DAQ_VERDICT_PASS;
+    DAQ_Verdict match_verdict = verdict_from_str(argv[1]);
+    std::string filter = concat_args(argc-2, argv+2);
+
+    DaqConfig pcap_config("pcap", "pcaps/dns.pcap", DAQ_MODE_READ_FILE, vars);
+    DataPlaneWorker wk0(pcap_config, 0, filter, match_verdict, default_verdict);
+    sleep(2);
+    wk0.stop();
+    wk0.join();
+
+    //DataPlaneWorker wk1(vpp_inline_config, 1, filter, match_verdict, default_verdict);
+    //wk1.stop(); wk1.join();
+
+    DAQ::unload_modules();
+    return 0;
+}
